b2284c5f14ed3847db363d42350ebf61af904c4a
CHARLES <83276284+charlie-hub-cell@users.noreply.github.com>
2025-03-24 19:14:51 +0300

    Update stkpush.php

--- stkpush.php
+++ stkpush.php
@@ -3,18 +3,18 @@
 include 'accessToken.php';
 date_default_timezone_set('Africa/Nairobi');
 $processrequestUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-$callbackurl = 'https://1c95-105-161-14-223.ngrok-free.app/MPEsa-Daraja-Api/callback.php';
+$callbackurl = 'https://regenessentialoils.com/darajaapp/callback.php';
 $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
 $BusinessShortCode = '174379';
 $Timestamp = date('YmdHis');
 // ENCRIPT  DATA TO GET PASSWORD
 $Password = base64_encode($BusinessShortCode . $passkey . $Timestamp);
-$phone = '';//phone number to receive the stk push
+$phone = '0748983441';//phone number to receive the stk push
 $money = '1';
 $PartyA = $phone;
 $PartyB = '254708374149';
-$AccountReference = 'UMESKIA SOFTWARES';
-$TransactionDesc = 'stkpush test';
+$AccountReference = 'TREC';
+$TransactionDesc = ' charles stkpush test';
 $Amount = $money;
 $stkpushheader = ['Content-Type:application/json', 'Authorization:Bearer ' . $access_token];
 //INITIATE CURL
